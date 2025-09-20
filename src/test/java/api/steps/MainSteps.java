40b360e30997807995d6a2b91553680ff147fd8d
Ильдар <ilda85@inbox.ru>
2025-09-20 11:54:35 +0500

    test(): добавление основных запросов с сайта reqres.in
    - Реализация запросов и ответов и их проверка статусов

--- src/test/java/api/steps/MainSteps.java
+++ src/test/java/api/steps/MainSteps.java
@@ -31,6 +31,87 @@ public class MainSteps {
         LOG.info("<<listSingleUser");
     }
 
+    @Step("Получаем список вещей")
+    public void listSingleUsers() {
+        LOG.info(">>listSingleUsers");
+        Response response = getListResources();
+        debugLogResponseAndCheckStatus(response);
+        LOG.debug("Ответ получен");
+        LOG.info("<<listSingleUsers");
+    }
+
+    @Step("Получаем одну вещь по id")
+    public void singleResource(int id) {
+        LOG.info(">>singleResource");
+        Response response = getSingleResource(id);
+        debugLogResponseAndCheckStatus(response);
+        LOG.debug("Ответ получен");
+        LOG.info("<<singleResource");
+    }
+
+    @Step("Создаем пользователя")
+    public void createSingleUser() {
+        LOG.info(">>createSingleUser");
+        Response response = postCreateUser();
+        debugLogResponseAndCheckStatus(response);
+        LOG.debug("Ответ получен");
+        LOG.info("<<createSingleUser");
+    }
+
+    @Step("Обновляем статус пользователя")
+    public void updateUser(int userId) {
+        LOG.info(">>updateUser");
+        Response response = putUpdateUser(userId);
+        debugLogResponseAndCheckStatus(response);
+        LOG.debug("Ответ получен");
+        LOG.info("<<updateUser");
+    }
+
+    @Step("Частичное обновление статуса пользователя")
+    public void patchUpdaterUser(int userId) {
+        LOG.info(">>patchUpdaterUser");
+        Response response = patchUpdateUser(userId);
+        debugLogResponseAndCheckStatus(response);
+        LOG.debug("Ответ получен");
+        LOG.info("<<patchUpdaterUser");
+    }
+
+    @Step("Удаление пользователя")
+    public void deleteSingleUser(int userId) {
+        LOG.info(">>deleteSingleUser");
+        Response response = deleteUser(userId);
+        debugLogResponseAndCheckStatus(response);
+        LOG.debug("Ответ получен");
+        LOG.info("<<deleteSingleUser");
+    }
+
+    @Step("Регистрация пользователя")
+    public void registeringUser() {
+        LOG.info(">>registeringUser");
+        Response response = registerUser();
+        debugLogResponseAndCheckStatus(response);
+        LOG.debug("Ответ получен");
+        LOG.info("<<registeringUser");
+    }
+
+    @Step("Логирование пользователя")
+    public void getLoginToken() {
+        LOG.info(">>getLoginToken");
+        Response response = postAndGetLoginToken();
+        debugLogResponseAndCheckStatus(response);
+        LOG.debug("Ответ получен");
+        LOG.info("<<getLoginToken");
+    }
+
+    @Step("Получение отложенного запроса и списка пользователей")
+    public void delayedResponse(int userId) {
+        LOG.info(">>delayedResponse");
+        Response response = getDelayedResponse(userId);
+        debugLogResponseAndCheckStatus(response);
+        LOG.debug("Ответ получен");
+        LOG.info("<<delayedResponse");
+    }
+
     private Response getListUsers() {
         RequestSpecification request = requestBaseBuilder
                 .buildCollectingSpecification();
@@ -39,6 +120,7 @@ public class MainSteps {
         }
         return request.when().get("/users?page=2");
     }
+
     private Response getSingleUser(int number) {
         RequestSpecification request = requestBaseBuilder
                 .buildCollectingSpecification();
@@ -46,6 +128,107 @@ public class MainSteps {
             request.log().all();
         }
         return request.when().get("/users/" + number);
+    }
+
+    private Response getListResources() {
+        RequestSpecification request = requestBaseBuilder
+                .buildCollectingSpecification();
+        if (LOG.isDebugEnabled()) {
+            request.log().all();
+        }
+        return request.when().get("/unknown");
+    }
+
+    private Response getSingleResource(int id) {
+        RequestSpecification request = requestBaseBuilder
+                .buildCollectingSpecification();
+        if (LOG.isDebugEnabled()) {
+            request.log().all();
+        }
+        return request.when().get("/unknown/" + id);
+    }
+
+    private Response postCreateUser() {
+        String body = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\"\n" +
+                "}";
+        RequestSpecification request = requestBaseBuilder
+                .buildCollectingSpecification();
+        if (LOG.isDebugEnabled()) {
+            request.log().all();
+        }
+        return request.body(body).when().post("/users");
+    }
+
+    private Response putUpdateUser(int userId) {
+        String body = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"zion resident\"\n" +
+                "}";
+        RequestSpecification request = requestBaseBuilder
+                .buildCollectingSpecification();
+        if (LOG.isDebugEnabled()) {
+            request.log().all();
+        }
+        return request.body(body).when().put("/users/" + userId);
+    }
+
+    private Response patchUpdateUser(int userId) {
+        String body = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"zion resident\"\n" +
+                "}";
+        RequestSpecification request = requestBaseBuilder
+                .buildCollectingSpecification();
+        if (LOG.isDebugEnabled()) {
+            request.log().all();
+        }
+        return request.body(body).when().patch("/users/" + userId);
+    }
+
+    private Response deleteUser(int userId) {
+        RequestSpecification request = requestBaseBuilder
+                .buildCollectingSpecification();
+        if (LOG.isDebugEnabled()) {
+            request.log().all();
+        }
+        return request.when().delete("/users/" + userId);
+    }
+
+    private Response registerUser() {
+        String body = "{\n" +
+                "    \"email\": \"eve.holt@reqres.in\",\n" +
+                "    \"password\": \"pistol\"\n" +
+                "}";
+        RequestSpecification request = requestBaseBuilder
+                .buildCollectingSpecification();
+        if (LOG.isDebugEnabled()) {
+            request.log().all();
+        }
+        return request.body(body).when().post("/register");
+    }
+
+    private Response postAndGetLoginToken() {
+        String body = "{\n" +
+                "    \"email\": \"eve.holt@reqres.in\",\n" +
+                "    \"password\": \"cityslicka\"\n" +
+                "}";
+        RequestSpecification request = requestBaseBuilder
+                .buildCollectingSpecification();
+        if (LOG.isDebugEnabled()) {
+            request.log().all();
+        }
+        return request.body(body).when().post("/login");
+    }
+
+    private Response getDelayedResponse(int userId) {
+        RequestSpecification request = requestBaseBuilder
+                .buildCollectingSpecification();
+        if (LOG.isDebugEnabled()) {
+            request.log().all();
+        }
+        return request.when().get("/users?delay=" + userId);
     }
 
     public void debugLogResponseAndCheckStatus(Response response) {
